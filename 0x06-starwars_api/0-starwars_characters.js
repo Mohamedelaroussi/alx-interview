45d62fcc540569899e0d9af7d5b0d925339844d9
‘Mohamedelaroussi’ <midmoha93@gmail.com>
2024-09-04 11:31:37 +0100

    fiiles

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -34,6 +34,35 @@ if (process.argv.length > 2) {
     // Wait for all Promises to resolve and log the names of the characters, separated by new lines
     Promise.all(charactersName)
       .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
+#!/usr/bin/node
+
+const request = require('request');
+const ID = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${ID}/`;
+
+function getCharacter (listChar, index) {
+  if (index === listChar.length) {
+    return;
+  }
+  request(`${listChar[index]}`, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const character = JSON.parse(response.body);
+      console.log(character.name);
+      getCharacter(listChar, index + 1);
+    }
+  });
+}
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const film = JSON.parse(response.body);
+    const listChar = film.characters;
+    getCharacter(listChar, 0);
+  }
+});
   });
 }
